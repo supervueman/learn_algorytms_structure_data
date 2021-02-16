084cbbbe2aec578d2d794321ab1ab58a5d97a936
supervueman <supervueman@gmail.com>
2021-02-16 08:41:25 +0400

    Add recrcive func find max num

--- recurcive.js
+++ recurcive.js
@@ -6,20 +6,28 @@ const fact = (x) => {
   }
 }
 
-console.log(fact(20))
+console.log(fact(20));
 
 const sumFunc = (arr, i) => {
   return i === arr.length ? 0 : arr[i] + sumFunc(arr, i + 1);
-}
+};
 
-const arrNums = [1, 2, 3, 4, 5]
+const arrNums = [1, 2, 3, 4, 5];
 
-console.log('Summ:', sumFunc(arrNums, 0))
+console.log('Summ:', sumFunc(arrNums, 0));
 
 const elemCount = (arr, i = 0) => {
   return arr[i] ? elemCount(arr, i + 1) : i;
-}
+};
 
-const arrNumsElemCount = [1, 2, 3, 4, 5]
+const arrNumsElemCount = [1, 2, 3, 4, 5];
 
 console.log('Elem count:', elemCount(arrNumsElemCount));
+
+const maxNum = (arr, i = 0) => {
+  return arr[i] && arr[i] < arr[i + 1] ? maxNum(arr, i + 1) : arr[i];
+};
+
+const arrNumsMaxNum = [1, 2, 10, 4, 5];
+
+console.log('Max num:', maxNum(arrNumsMaxNum));
